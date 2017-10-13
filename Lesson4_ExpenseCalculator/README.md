84cd15cbcc2e485cea83463bcfabc936c0491362
Zachary Birchall <zjb16@my.fsu.edu>
2017-10-13 23:47:34 +0000

    lesson 4 final commit

--- Lesson4_ExpenseCalculator/README.md
+++ Lesson4_ExpenseCalculator/README.md
@@ -4,20 +4,19 @@
 Web Application Development
 
 ### Assignment Date:  
-(Month Day, Year)
+(October 13, 2017)
 
 ### Student Name:  
-(First and Last Name)
+(Zachary Birchall)
 
 ### Project Description:
-(In your own words, briefly describe the assignment.)
+(This assignment is for us to practice assigning variables and seeing the program change as we work on it. It is also to practice using the getElementById program.)
 
 ### View Project:
-(Replace this statement with your GitHub Page URL that was created when you 
- published the project.)
+(https://zbirchall.github.io/lesson4_javascript1/)
 
 ### Lessons Learned in the Assignment:
-1. (Briefly describe a lesson/concept learned in this lesson.)
-2. (Briefly describe a lesson/concept learned in this lesson.)
-3. (Briefly describe a lesson/concept learned in this lesson.)
+1. (I learned that trying to breeze through the assignments is not a good idea. I messed up multiple times and had to go back to change what i missed.)
+2. (I learned how to use the getElementById program and what it is used for.)
+3. (I learned how to assign variables and use the, to do other things.)
 
--- Lesson4_ExpenseCalculator/expenseCalculator.js
+++ Lesson4_ExpenseCalculator/expenseCalculator.js
@@ -1,8 +1,8 @@
         /************************************************************************************************ 
           Course:  Web Application Development
           Assignment:  Lesson 4 (JavaScript 1)
-          Author:  
-          Date:   
+          Author:  Zachary Birchall
+          Date:   October 13, 2017
           Filename: expenseCalculator.js
         *************************************************************************************************/
      
@@ -36,6 +36,9 @@
 
                   var shelterAmt = document.getElementById("shelterInput").value;
                   var foodAmt = document.getElementById("foodInput").value;
+                  var householdAmt = document.getElementById("householdInput").value;
+                  var miscellaneousAmt = document.getElementById("miscellaneousInput").value;
+                  var transportationAmt = document.getElementById("transportationInput").value;
 
 
 
@@ -58,6 +61,9 @@
 
                   shelterAmt = parseFloat(shelterAmt);
                   foodAmt = parseFloat(foodAmt);
+                  householdAmt = parseFloat(householdAmt);
+                  miscellaneousAmt = parseFloat(miscellaneousAmt);
+                  transportationAmt = parseFloat(transportationAmt);
 
 
 
@@ -79,6 +85,9 @@
                   var totalExpenses; //total expenses for the user
                   var shelterPct; //shelter category percentage
                   var foodPct;  //food category percentage
+                  var householdPct;  //household category percentage
+                  var miscellaneousPct;  //miscellaneous category percentage
+                  var transportationPct;  //transportation category percentage
 
 
 
@@ -105,10 +114,13 @@
                    *          Miscellaneous percentage (you must add). The result of the calculations are assigned to the 
                    *          variables declared in Task 4.  
                   *******************************************************************************************************/   
-
-
+                  
+                  totalExpenses = shelterAmt + foodAmt + householdAmt + miscellaneousAmt + transportationAmt;
                   shelterPct = 100 * (shelterAmt / totalExpenses);
                   foodPct = 100 * (foodAmt / totalExpenses);
+                  householdPct = 100 * (householdAmt / totalExpenses);
+                  miscellaneousPct = 100 * (miscellaneousAmt / totalExpenses);
+                  transportationPct = 100 * (transportationAmt / totalExpenses);
 
 
                   /******************************************************************************************************
@@ -135,6 +147,10 @@
                   document.getElementById("totalExpenses").innerHTML = "<b>Total Expenses:     $</b> " + totalExpenses.toFixed(2);
                   document.getElementById("shelterPercentage").innerHTML = "% for <b>shelter</b>:      " + shelterPct.toFixed(1);      
                   document.getElementById("foodPercentage").innerHTML = "% for <b>food:</b>      " + foodPct.toFixed(1);
+                  document.getElementById("householdPercentage").innerHTML = "% for <b>household:<b>      " + householdPct.toFixed(1);
+                  document.getElementById("miscellaneousPercentage").innerHTML = "% for <b>miscellaneous:<b>      " + miscellaneousPct.toFixed(1);
+                  document.getElementById("transportationPercentage").innerHTML = "% for <b>transportation:<b>      " + transportationPct.toFixed(1);
+                  
 
 
          } //end of calculatePercentages function
@@ -171,6 +187,9 @@
                   document.getElementById("totalExpenses").innerHTML = ""; 
                   document.getElementById("shelterPercentage").innerHTML = "";      
                   document.getElementById("foodPercentage").innerHTML  = "";
+                  document.getElementById("householdPercentage").innerHTML  = "";
+                  document.getElementById("miscellaneousPercentage").innerHTML  = "";
+                  document.getElementById("transportationPercentage").innerHTML  = "";
 
 
               
